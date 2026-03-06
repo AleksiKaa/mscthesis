1a38244659e9a74aa9f22873ffe8b97fe96e7c70
Aleksi Kääriäinen <allu.kaariainen@gmail.com>
2026-03-06 16:36:02 +0200

    increase resourcing

--- src/batch_jobs/generate_batch.sh
+++ src/batch_jobs/generate_batch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --chdir=/home/kaariaa3/mscthesis
-#SBATCH --time=00:15:00
+#SBATCH --time=02:00:00
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16GB # This is system memory, not GPU memory.
 #SBATCH --gpus=1
-#SBATCH --gres=min-vram:32g
-#SBATCH --output ./outputs/jobs/batch.%J.out
-#SBATCH --error ./outputs/errs/batch.%J.err
+#SBATCH --gres=min-vram:80g
+#SBATCH --output ./outputs/jobs/batch.%j.out
+#SBATCH --error ./outputs/errs/batch.%j.err
 
 module purge
 module load model-huggingface
