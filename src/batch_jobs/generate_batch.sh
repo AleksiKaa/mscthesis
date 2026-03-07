caa6735935193540a0d449150d93c388fed0ed06
Aleksi Kääriäinen <allu.kaariainen@gmail.com>
2026-03-07 12:01:21 +0200

    reserve more ram

--- src/batch_jobs/generate_batch.sh
+++ src/batch_jobs/generate_batch.sh
@@ -2,7 +2,7 @@
 #SBATCH --chdir=/home/kaariaa3/mscthesis
 #SBATCH --time=02:00:00
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=16GB # This is system memory, not GPU memory.
+#SBATCH --mem=32GB # This is system memory, not GPU memory.
 #SBATCH --gpus=1
 #SBATCH --gres=min-vram:80g
 #SBATCH --output ./outputs/jobs/batch.%j.out
