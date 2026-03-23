380887e4afa4e51fda6285a379471d5fcb5f69e4
Aleksi Kääriäinen <allu.kaariainen@gmail.com>
2026-03-23 19:03:28 +0200

    Write main start point

--- src/main.py
+++ src/main.py
@@ -1,1 +1,26 @@
-print("Hello world")
+import os
+import sys
+import subprocess
+from time import sleep
+
+os.chdir("/home/kaariaa3/mscthesis")
+sys.path.append("./src/")  # Add module directory to path
+
+
+def main():
+    submit_script = "./src/submit.sh"
+
+    runs = [
+        "-t d",  # zero-shot
+        "-t d -rd 1",  # 1-shot with random sampling
+        "-t d -rd 5",  # few-shot with 5 random samples
+        "-t d -ufd True",  # few-shot with 5 hand-picked examples
+    ]
+
+    for run in runs:
+        subprocess.call([submit_script, "-p", run])
+        sleep(1)
+
+
+if __name__ == "__main__":
+    main()
