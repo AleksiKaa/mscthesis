c4d4a375e6a99e99e800cd594bc02617d61f7e4e
Aleksi Kääriäinen <allu.kaariainen@gmail.com>
2026-04-04 21:59:14 +0300

    Add script for checking status of slurm jobs

---
+++ get_slurm_status.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# This script checks the status of SLURM jobs for different models by looking
+# for error logs and counting the number of started, finished, and failed jobs.
+
+models=(
+    "Qwen/Qwen3-8B"
+    "Qwen/Qwen3-32B"
+    "meta-llama/Llama-3.1-8B-Instruct"
+    "meta-llama/Llama-3.3-70B-Instruct"
+    "mistralai/Mistral-7B-Instruct-v0.3"
+    "mistralai/Mistral-Small-3.2-24B-Instruct-2506"
+)
+
+for model in "${models[@]}"; do
+
+    failed_jobs=($(grep -HRls 'Traceback (most recent call last):' ./outputs/$1/$model/*/*.err))
+
+    echo "Model: $model"
+    echo "Number of started jobs: $(find . -path "./outputs/$1/$model/*/*.json" | wc -l)"
+    echo "Number of finished jobs: $(find . -path "./outputs/$1/$model/*/*.csv" | wc -l)"
+    echo "Number of failed jobs: ${#failed_jobs[@]}"
+
+    if [ ${#failed_jobs[@]} -ne 0 ]; then
+        echo "Failed job details:"
+        for job in "${failed_jobs[@]}"; do
+            echo "Job ID: $(basename $(dirname $job))"
+        done
+    fi
+    echo "-----------------------------"
+
+done
