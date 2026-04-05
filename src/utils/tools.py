30d0930872d05d380c3a9da8962d9683e2d9a7a8
Aleksi Kääriäinen <allu.kaariainen@gmail.com>
2026-04-05 12:12:15 +0300

    Format table col, row names

--- src/utils/tools.py
+++ src/utils/tools.py
@@ -1,6 +1,8 @@
 import json
+import re
 from pathlib import Path
 
+import pandas as pd
 
 def get_config(path):
     config = None
@@ -73,5 +75,102 @@ def prettify_table(df):
     )
     # When num is 0, remove value of type
     df.loc[df["number_of_demonstrations"] == 0, "type_of_demonstrations"] = "none"
+
+    return df
+
+
+def bold_extreme_values(s, by_model=True):
+    # Bold max for mean
+
+    if 'mean' not in s.name and 'std' not in s.name:
+        return ['' for v in s]
+
+
+    if not by_model:
+        if "mean" in s.name:
+            is_max = s == s.max()
+            return ['font-weight: bold' if v else '' for v in is_max]
+        if "std" in s.name:
+            is_min = s == s.min()
+            return ['font-weight: bold' if v else '' for v in is_min]
+    
+    font_array = []
+
+    model_level = s.index.names.index('model')
+    models = s.index.get_level_values(model_level)
+    models = pd.Series(list(models)).unique()
+
+    idx = pd.IndexSlice
+    
+    for model in models:   
+        values_by_model = s.loc[idx[model]]
+        
+        if "mean" in s.name:
+            is_max = values_by_model == values_by_model.max()
+            font_array += ['font-weight: bold' if v else '' for v in is_max]
+        if "std" in s.name:
+            is_min = values_by_model == values_by_model.min()
+            font_array += ['font-weight: bold' if v else '' for v in is_min]
+
+    return font_array
+
+
+def aggregate_results(df, by_cols, cols, funs=None):
+    grouped = df.groupby(
+        by=by_cols,
+        as_index=True,
+    )
+    
+    if funs is None:
+        funs = [
+        "mean",
+        "std",
+        "count"
+    ]
+
+    agg = grouped.agg({col: funs for col in cols})
+
+    return agg
+
+
+def format_table(df):
+    """
+    Replace '_' with ' ' in all levels of both columns and index.
+    Take only model family name and param count for models.
+    """
+    df = df.copy()
+
+    def _format_model_name(name):
+        parts = name.split("-")
+        match = re.search(r"\d{1,2}B", name)
+        return f"{parts[0]}-{match.group(0)}" if match else name
+
+    def _replace_in_index(idx):
+        if isinstance(idx, pd.MultiIndex):
+            return pd.MultiIndex.from_tuples(
+                [
+                    tuple(
+                        str(level).replace("_", " ") if level is not None else level
+                        for level in tup
+                    )
+                    for tup in idx
+                ],
+                names=[name.replace('_', ' ') if name is not None else None for name in idx.names]
+            )
+        else:
+            return pd.Index(
+                [
+                    str(val).replace("_", " ") if val is not None else val
+                    for val in idx
+                ],
+                name=idx.name
+            )
+
+    df.columns = _replace_in_index(df.columns)
+    df.index = _replace_in_index(df.index)
+
+    model_level = df.index.names.index('model')
+    models = df.index.get_level_values(model_level).unique()
+    df.index = df.index.set_levels(levels=[_format_model_name(model) for model in models], level=model_level)
 
     return df
